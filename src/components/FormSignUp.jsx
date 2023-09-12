d46211efab57703cf105d0b78c756604f4583e42
JonnyMarin96 <jonny.yair.2@gmail.com>
2023-09-11 20:01:44 -0600

    Se agrego libreria Material UI

--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -1,8 +1,26 @@
+import { Button } from "@mui/material"
+
 function FormSignup(){
-    return <form>
-        <input type="text"/>
-        <button> Registrar </button>
-    </form>
+    return (
+        <form>
+            <label> Nombre </label>
+            <input type="text"/>
+
+            <label> Apellido </label>
+            <input type="text"/>
+
+            <label> E-mail </label>
+            <input type="email"/>
+
+            <label> Promociones </label>
+            <input type="checkbox"/>
+
+            <label> Novedades </label>
+            <input type="checkbox"/>
+
+            <Button variant="contained"> Registrar </Button>
+        </form>
+    )
 }
 
 export default FormSignup
