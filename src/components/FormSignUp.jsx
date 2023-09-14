8240c8d967aab7a2efee809ea64ecd6f47e44490
JonnyMarin96 <jonny.yair.2@gmail.com>
2023-09-13 19:47:57 -0600

    Se agrego el uso de estados

--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -1,15 +1,41 @@
 import { Button, Switch, FormGroup, FormControlLabel } from "@mui/material"
+import { useState } from "react";
 
 import TextField from '@mui/material/TextField';
 
+
+
 function FormSignup(){
+
+    const [name, setName] = useState("QWERTY") 
+
     return (
         <form>
-            <TextField id="name" label="Nombre" variant="outlined" fullWidth={true} margin="normal"/>
+            <TextField 
+                id="name" 
+                label="Nombre" 
+                variant="outlined" 
+                fullWidth={true} 
+                margin="normal" 
+                onChange={(e) => setName(e.target.value)} 
+                value={name}
+            />
 
-            <TextField id="last-name" label="Apellido" variant="outlined" fullWidth={true} margin="normal"/>
+            <TextField 
+                id="last-name" 
+                label="Apellido" 
+                variant="outlined" 
+                fullWidth={true} 
+                margin="normal"
+            />
 
-            <TextField id="email" label="E-mail" variant="outlined" fullWidth={true} margin="normal"/>
+            <TextField 
+                id="email" 
+                label="E-mail" 
+                variant="outlined" 
+                fullWidth={true} 
+                margin="normal"
+            />
 
             <FormGroup>
                 <FormControlLabel control={
