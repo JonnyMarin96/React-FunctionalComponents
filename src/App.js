c9cfaae8d08281dac46594fa1443a01362e42a7a
JonnyMarin96 <jonny.yair.2@gmail.com>
2023-09-13 19:05:46 -0600

    Se agregó container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import { Container } from '@mui/material';
 import FormSignup from './components/FormSignUp';
 
 function App() {
   return (
     <>
-      <h1> Formulario Registro </h1>
-      <FormSignup/>
+      <Container component="section" maxWidth="sm">
+        <h1> Formulario Registro </h1>
+        <FormSignup/>
+      </Container>
     </>
   );
 }
