19c71565bd03765a25b67e0c26e1d6a3de57567f
JonnyMarin96 <jonny.yair.2@gmail.com>
2023-09-14 22:27:47 -0600

    Se agrego validacion de un campo

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import { Container, Typography } from '@mui/material';
 import FormSignup from './components/FormSignUp';
 
 function App() {
+
+  const handleSubmit = (valores) => {
+    console.log("App JS: ", valores)
+  }
+
   return (
     <>
       <Container component="section" maxWidth="sm">
         <Typography variant='h3' align='center'>Formulario Registro</Typography>
-        <FormSignup/>
+        <FormSignup handleSubmit={handleSubmit}/>
       </Container>
     </>
   );
--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -5,12 +5,45 @@ import TextField from '@mui/material/TextField';
 
 
 
-function FormSignup(){
+function FormSignup(props){
 
-    const [name, setName] = useState("QWERTY") 
+    const { handleSubmit } = props
+
+    const [name, setName] = useState("") 
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("") 
+    const [nov, setNov] = useState(false)
+    const [promo, setPromo] = useState(true)
+    const [errors, setErrors] = useState({
+        name: {
+            error: false,
+            message: ""
+        }
+    })
+
+    const validarNombre = (nombre) => {
+        if(nombre.length >= 3){
+            return {
+                name: {
+                    error: false,
+                    message: ''
+                }
+            }
+        }else{
+            return {
+                name: {
+                    error: true,
+                    message: "Deben ser al menos 3 caracteres"
+                }
+            }
+        }
+    }
 
     return (
-        <form>
+        <form on onSubmit={(e) => {
+            e.preventDefault()
+            handleSubmit({name, lastName, email, nov, promo})
+        }}>
             <TextField 
                 id="name" 
                 label="Nombre" 
@@ -19,6 +52,11 @@ function FormSignup(){
                 margin="normal" 
                 onChange={(e) => setName(e.target.value)} 
                 value={name}
+                error = {errors.name.error}
+                helperText = {errors.name.error ? errors.name.message : ''}
+                onBlur={(e) => {
+                    setErrors( validarNombre(e.target.value) )
+                }}
             />
 
             <TextField 
@@ -27,6 +65,8 @@ function FormSignup(){
                 variant="outlined" 
                 fullWidth={true} 
                 margin="normal"
+                onChange={(e) => setLastName(e.target.value)}
+                value={lastName}
             />
 
             <TextField 
@@ -35,19 +75,25 @@ function FormSignup(){
                 variant="outlined" 
                 fullWidth={true} 
                 margin="normal"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
             />
 
             <FormGroup>
                 <FormControlLabel control={
-                    <Switch/>
+                    <Switch checked={promo} onChange={
+                        (e) => setPromo(e.target.checked)
+                    }/>
                 } label="Promociones"/>
 
-            <FormControlLabel control={
-                    <Switch/>
+                <FormControlLabel control={
+                    <Switch checked={nov} onChange={
+                        (e) => setNov(e.target.checked)
+                    }/>
                 } label="Novedades"/>
             </FormGroup>
 
-            <Button variant="contained"> Registrar </Button>
+            <Button variant="contained" type="submit"> Registrar </Button>
         </form>
     )
 }
